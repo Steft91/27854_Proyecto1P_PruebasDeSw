85593c2780e6391b82d5dceac1bd24d957d3f26b
IMarcusDev <escobarmarcos511@gmail.com>
2025-11-11 09:29:17 -0500

    feature(app): Added clients route to app.js for use

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const clientRoutes = require('./routes/cliente.routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/clients', clientRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+});
+
+module.exports = app;
