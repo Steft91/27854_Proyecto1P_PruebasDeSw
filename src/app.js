b2d3ad45abd7c1617da349d05994049b3afda6f2
adarboleda <adarboleda@espe.edu.ec>
2025-11-14 18:24:25 -0500

    Proveedor app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const clientRoutes = require('./routes/cliente.routes');
+const providerRoutes = require('./routes/proveedor.routes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/clients', clientRoutes);
-
+app.use('/api/providers', providerRoutes);
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Ruta no encontrada' });
+  res.status(404).json({ message: 'Ruta no encontrada' });
 });
 
 module.exports = app;
