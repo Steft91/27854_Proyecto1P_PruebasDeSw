7872e4b31920e5b905f7dc1fe1b4562c79de771e
Gabriel Nicolas Vivanco Raza <150310839+GabrielNicolasVivancoRaza@users.noreply.github.com>
2025-11-15 12:29:35 -0500

    Cambios app

--- supermercado-backend/src/app.js
+++ supermercado-backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const clientRoutes = require('./routes/cliente.routes');
 const providerRoutes = require('./routes/proveedor.routes');
+const empleadoRoutes = require('./routes/empleado.routes');
+
 
 const app = express();
 
@@ -8,6 +10,7 @@ app.use(express.json());
 
 app.use('/api/clients', clientRoutes);
 app.use('/api/providers', providerRoutes);
+app.use('/api/empleados', empleadoRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Ruta no encontrada' });
