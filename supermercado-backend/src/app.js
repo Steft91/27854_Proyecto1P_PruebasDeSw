502982f5fdaf500ed516054ab474580bfeaeb752
Steft91 <stefany152005@gmail.com>
2025-11-15 15:58:20 -0500

    app actualizada con producto

--- supermercado-backend/src/app.js
+++ supermercado-backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const clientRoutes = require('./routes/cliente.routes');
 const providerRoutes = require('./routes/proveedor.routes');
 const empleadoRoutes = require('./routes/empleado.routes');
+const productoRoutes = require('./routes/producto.routes');
 
 
 const app = express();
@@ -11,6 +12,9 @@ app.use(express.json());
 app.use('/api/clients', clientRoutes);
 app.use('/api/providers', providerRoutes);
 app.use('/api/empleados', empleadoRoutes);
+app.use('/api/products', productoRoutes);
+
+// Manejo de rutas no encontradas
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Ruta no encontrada' });
